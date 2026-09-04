5c17011730b28a8f0c3ef0a2f38c1326b1b99200
Prashant Kumar Rajhans <prashantbabu980123@gmail.com>
2026-09-04 21:18:12 +0530

    Time: 5 ms (37.02%), Space: 43.4 MB (83.42%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,26 +1,18 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         StringBuilder result = new StringBuilder();
-        int balance = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (c == '(') {
-                // If balance is > 0, it means this is not an outermost '('
-                if (balance > 0) {
-                    result.append(c);
-                }
-                balance++;
-            } else {
-                balance--;
-                // If balance is > 0 after decrementing, it means this is not an outermost ')'
-                if (balance > 0) {
-                    result.append(c);
-                }
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++){
+            if(s.charAt(i)==')'){
+                counter--;
+            }
+            if(counter!=0){
+                result.append(s.charAt(i));
+            }
+            if(s.charAt(i)=='('){
+                counter++;
             }
         }
-        
         return result.toString();
     }
 }
