8674bc4673dd091dede379b9fa2d9da1af31c311
Prashant Kumar Rajhans <prashantbabu980123@gmail.com>
2026-09-07 20:03:49 +0530

    Time: 4 ms (83.67%), Space: 44.1 MB (94.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,24 +3,42 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character, Integer> map = new HashMap<>();
-        int count = 1;
-        for(int i=0; i<s.length(); i++){
-            char ch1 = s.charAt(i);
-            if (map.containsKey(ch1)) {
-                map.put(ch1, map.get(ch1) + 1);
-            } else {
-                map.put(ch1, 1);
-            } 
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // int count = 1;
+        // for(int i=0; i<s.length(); i++){
+        //     char ch1 = s.charAt(i);
+        //     if (map.containsKey(ch1)) {
+        //         map.put(ch1, map.get(ch1) + 1);
+        //     } else {
+        //         map.put(ch1, 1);
+        //     } 
+        // }
+        // for(int i=0; i<t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     if(!map.containsKey(ch)){
+        //         return false;
+        //     }else{
+        //         map.put(ch, map.get(ch)-1);
+        //     }
+        //     if (map.get(ch) < 0) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //Using HashArray
+        int[] count = new int[26];
+        
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            count[ch-97]++;
         }
-        for(int i=0; i<t.length(); i++){
+        for(int i=0; i<t.length();i++){
             char ch = t.charAt(i);
-            if(!map.containsKey(ch)){
-                return false;
-            }else{
-                map.put(ch, map.get(ch)-1);
-            }
-            if (map.get(ch) < 0) {
+            count[ch-97]--;
+        }
+        for(int val:count){
+            if(val!=0){
                 return false;
             }
         }
